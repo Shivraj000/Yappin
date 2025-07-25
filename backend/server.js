6c836ef3b49a73b9ddc49b0bc6ba42474957928e
Shivraj000 <amshivraj2002@gmail.com>
2025-07-25 22:13:30 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config(); 
@@ -9,7 +10,7 @@ import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
-
+const __dirname = path.resolve();
 
 
 
@@ -21,6 +22,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
